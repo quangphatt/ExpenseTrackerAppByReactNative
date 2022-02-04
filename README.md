911d24985e62112a4241682f62732fe2ec282c2c
Quang Phat <phatquang2811@gmail.com>
2022-02-04 21:04:11 +0700

    setup project

---
+++ README.md
@@ -0,0 +1,2 @@
+## Expense Tracker App by React Native
+Reference: https://www.youtube.com/watch?v=uBcpWOQqbAQ
---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View 
+} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Home;
+
