c02617a970b16a6b7f6fca7b25925f8ce9a21e62
Quang Phat <phatquang2811@gmail.com>
2022-02-06 21:10:25 +0700

    add Chart

--- screens/Home.js
+++ screens/Home.js
@@ -11,6 +11,10 @@ import {
     Platform
 } from 'react-native';
 
+import { VictoryPie } from 'victory-native';
+
+import {Svg} from 'react-native-svg';
+
 import { COLORS, FONTS, SIZES, icons, images } from '../constants';
 
 const Home = () => {
@@ -597,12 +601,149 @@ const Home = () => {
         );
     }
 
+    const processCategoryDataToDisplay = () => {
+        let chartData = categories.map((item) => {
+            let confirmExpenses = item.expenses.filter(a => a.status == "C");
+            var total = confirmExpenses.reduce((a, b) => a + (b.total || 0), 0);
+
+            return {
+                name: item.name,
+                y: total,
+                expenseCount: confirmExpenses.length,
+                color: item.color,
+                id: item.id
+            }
+        });
+
+        let filterChartData = chartData.filter(a => a.y > 0);
+
+        let totalExpense = filterChartData.reduce((a, b) => a + (b.y || 0), 0);
+
+        let finalChartData = filterChartData.map((item) => {
+            let percentage = (item.y / totalExpense * 100).toFixed(0);
+            return {
+                label: `${percentage}%`,
+                y: Number(item.y),
+                expenseCount: item.expenseCount,
+                color: item.color,
+                name: item.name,
+                id: item.id
+            }
+        });
+
+        return finalChartData;
+    }
+
+    const setSelectCategoryByName = (name) => {
+        let category = categories.filter(a => a.name == name);
+        setSelectedCategory(category[0]);
+    }
+
     const renderChart = () => {
-        return (
-            <View>
-                <Text>Chart</Text>
-            </View>
-        );
+        let chartData = processCategoryDataToDisplay();
+        let colorScales = chartData.map((item) => item.color);
+        let totalExpenseCount = chartData.reduce((a, b) => a + (b.expenseCount || 0), 0);
+
+        console.log("Check Chart");
+        console.log(chartData);
+
+        if(Platform.OS==='ios'){
+            return (
+                <View style={{
+                    alignItems: 'center', 
+                    justifyContent: 'center',
+                }}>
+                    <VictoryPie
+                        data={chartData}
+                        labels={(datum) => `${datum.y}`}
+                        radius={({ datum }) => (selectedCategory && selectedCategory.name == datum.name) ? SIZES.width * 0.4 : SIZES.width * 0.4 - 10}
+                        innerRadius={70}
+                        labelRadius={({ innerRadius }) => (SIZES.width * 0.4 + innerRadius) / 2.5}
+                        style={{
+                            labels: { fill: "white", ...FONTS.body3 },
+                            parent: {
+                                ...styles.shadow
+                            },
+                        }}
+                        width={SIZES.width * 0.8}
+                        height={SIZES.width * 0.8}
+                        colorScale={colorScales}
+                        events={[{
+                            target: "data",
+                            eventHandlers: {
+                                onPress: () => {
+                                    return [{
+                                        target: "labels",
+                                        mutation: (props) => {
+                                            let categoryName = chartData[props.index].name
+                                            setSelectCategoryByName(categoryName)
+                                        }
+                                    }]
+                                }
+                            }
+                        }]}
+                    />
+
+                    <View style={{ position: 'absolute', top: '42%', left: "42%" }}>
+                        <Text style={{ ...FONTS.h1, textAlign: 'center' }}>{totalExpenseCount}</Text>
+                        <Text style={{ ...FONTS.body3, textAlign: 'center' }}>Expenses</Text>
+                    </View>
+                </View>
+            );
+        } else {
+            return (
+                <View style={{
+                    alignItems: 'center', 
+                    justifyContent: 'center',
+                }}>
+                    <Svg
+                        width={SIZES.width} 
+                        height={SIZES.width} 
+                        style={{
+                            width: "100%", 
+                            height: "auto"
+                        }}
+                    >
+                        <VictoryPie 
+                            standalone={false}
+                            data={chartData}
+                            labels={(datum) => `${datum.y}`}
+                            radius={({ datum }) => (selectedCategory && selectedCategory.name == datum.name) ? SIZES.width * 0.4 : SIZES.width * 0.4 - 10}
+                            innerRadius={70}
+                            labelRadius={({ innerRadius }) => (SIZES.width * 0.4 + innerRadius) / 2.5}
+                            style={{
+                                labels: { fill: "white", ...FONTS.body3 },
+                                parent: {
+                                    ...styles.shadow
+                                },
+                            }}
+                            width={SIZES.width}
+                            height={SIZES.width}
+                            colorScale={colorScales}
+                            events={[{
+                                target: "data",
+                                eventHandlers: {
+                                    onPress: () => {
+                                        return [{
+                                            target: "labels",
+                                            mutation: (props) => {
+                                                let categoryName = chartData[props.index].name
+                                                setSelectCategoryByName(categoryName)
+                                            }
+                                        }]
+                                    }
+                                }
+                            }]}
+                        />
+                    </Svg>
+
+                    <View style={{ position: 'absolute', top: '42%', left: "42%" }}>
+                        <Text style={{ ...FONTS.h1, textAlign: 'center' }}>{totalExpenseCount}</Text>
+                        <Text style={{ ...FONTS.body3, textAlign: 'center' }}>Expenses</Text>
+                    </View>
+                </View>
+            );
+        }
     }
 
     const renderExpenseSummary = () => {
