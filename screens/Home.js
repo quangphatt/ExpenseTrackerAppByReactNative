9008c7bd23fc8c7658335defeec6309ce839c5a1
Quang Phat <phatquang2811@gmail.com>
2022-02-06 20:30:50 +0700

    add Incoming Expenses

--- screens/Home.js
+++ screens/Home.js
@@ -471,9 +471,128 @@ const Home = () => {
     }
 
     const renderIncomingExpenses = () => {
+        let allExpenses = selectedCategory ? selectedCategory.expenses : [];
+        let incomingExpenses = allExpenses.filter(a => a.status == "P");
+
+        const renderItem = ({item, index}) => (
+            <View
+                style={{
+                    width: 300,
+                    marginRight: SIZES.padding,
+                    marginLeft: index == 0 ? SIZES.padding : 0,
+                    marginVertical: SIZES.radius,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.white,
+                    ...styles.shadow,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row', 
+                        padding: SIZES.padding, 
+                        alignItems: 'center'
+                    }}
+                >
+                    <View
+                        style={{
+                            height: 50,
+                            width: 50,
+                            borderRadius: 25,
+                            backgroundColor: COLORS.lightGray,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginRight: SIZES.base
+                        }}
+                    >
+                        <Image 
+                            source={selectedCategory.icon}
+                            style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: selectedCategory.color
+                            }}
+                        />
+                    </View>
+
+                    <Text style={{...FONTS.h3, color: selectedCategory.color,}}>{selectedCategory.name}</Text>
+                </View>
+
+                <View
+                    style={{
+                        paddingHorizontal: SIZES.padding
+                    }}
+                >
+                    <Text style={{ ...FONTS.h2, }}>{item.title}</Text>
+                    <Text style={{ ...FONTS.body3, flexWrap: 'wrap', color: COLORS.darkgray }}>
+                        {item.description}
+                    </Text>
+
+                    <Text style={{ marginTop: SIZES.padding, ...FONTS.h4, }}>Location</Text>
+                    <View style={{flexDirection: 'row'}}>
+                        <Image
+                            source={icons.pin}
+                            style={{
+                                width: 20,
+                                height: 20,
+                                tintColor: COLORS.darkgray,
+                                marginRight: 5
+                            }}
+                        />
+                        <Text style={{ marginBottom: SIZES.base, color: COLORS.darkgray, ...FONTS.body4 }}>{item.location}</Text>
+                    </View>
+                </View>
+
+                <View
+                    style={{
+                        height: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderBottomStartRadius: SIZES.radius,
+                        borderBottomEndRadius: SIZES.radius,
+                        backgroundColor: selectedCategory.color,
+                    }}
+                >
+                    <Text style={{ color: COLORS.white, ...FONTS.body3 }}>CONFIRM {item.total.toFixed(2)} USD</Text>
+                </View>
+            </View>
+        )
+
         return (
             <View>
-                <Text>Incoming Expenses</Text>
+                <View
+                    style={{
+                        height: 80, 
+                        backgroundColor: COLORS.lightGray2, 
+                        padding: SIZES.padding,
+                    }}
+                >
+                    <Text style={{ ...FONTS.h3, color: COLORS.primary }}>INCOMING EXPENSES</Text>
+                    <Text style={{ ...FONTS.body4, color: COLORS.darkgray }}>12 Total</Text>
+                </View>
+
+                {
+                    incomingExpenses.length > 0 &&
+                    <FlatList 
+                        data={incomingExpenses}
+                        renderItem={renderItem}
+                        keyExtractor={item => `${item.id}`}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                    />
+                }
+
+                {
+                    incomingExpenses.length == 0 &&
+                    <View
+                        style={{
+                            alignItems: 'center', 
+                            justifyContent: 'center', 
+                            height: 300
+                        }}
+                    >
+                        <Text style={{ color: COLORS.primary, ...FONTS.h3 }}>No Record</Text>
+                    </View>
+                }
             </View>
         );
     }
